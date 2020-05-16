7f76eb0963cdb17d7c1026ba2b646caf9e5b2ff4
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-05-16 16:44:11 -0300

    Criado entidade Aluno

---
+++ src/br/com/javaaps/models/Aluno.java
@@ -0,0 +1,51 @@
+package br.com.javaaps.models;
+
+public class Aluno {
+	
+	private String id;
+	private String nome;
+	
+	public Aluno(String id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Identificador: %s - Nome: %s", getId(), getNome());
+	}
+}
